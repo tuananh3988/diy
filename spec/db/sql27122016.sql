fbdbce78ae10e234b3775269db8802d2573956be
Tuan anh - home <tuananh3988@gmail.com>
2016-12-27 02:24:12 +0700

    update db

--- spec/db/sql27122016.sql
+++ spec/db/sql27122016.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `mtb_token`
-ADD COLUMN `type`  tinyint(4) NULL DEFAULT 0 COMMENT '1:ios. 2: android' AFTER `uuid`;
+ADD COLUMN `type`  tinyint(4) NULL DEFAULT 1 COMMENT '1:ios. 2: android' AFTER `uuid`;
